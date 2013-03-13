90f89b2c7cd3b6b3eac04ba0e5e4390b1615acd7
Jack Wu <jack.nan.wu@gmail.com>
2013-03-13 00:54:31 -0700

    Added media sensor files

---
+++ SensorContainer/SensorContainer/Sensor/Device/Media/MediaSensor.h
@@ -0,0 +1,13 @@
+//
+//  MediaSensor.h
+//  SensorContainer
+//
+//  Created by Jack Wu on 13-03-12.
+//  Copyright (c) 2013 Daniel Yuen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MediaSensor : NSObject
+
+@end
---
+++ SensorContainer/SensorContainer/Sensor/Device/Media/MediaSensor.m
@@ -0,0 +1,13 @@
+//
+//  MediaSensor.m
+//  SensorContainer
+//
+//  Created by Jack Wu on 13-03-12.
+//  Copyright (c) 2013 Daniel Yuen. All rights reserved.
+//
+
+#import "MediaSensor.h"
+
+@implementation MediaSensor
+
+@end
